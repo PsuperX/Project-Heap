3a828227395f3a103060db4f1e1a8b7e29c2c635
PsuperX <50513933+PsuperX@users.noreply.github.com>
2019-12-27 10:20:45 +0000

    Camera Aiming

---
+++ Assets/Behaviour-Editor/Scripts/Behavior/Actions/HandleCameraZoom.cs
@@ -0,0 +1,41 @@
+﻿using SO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    [CreateAssetMenu(menuName ="Actions/Mono Actions/Camera Zoom")]
+    public class HandleCameraZoom : Action
+    {
+        [SerializeField] TransformVariable actualCameraTrans;
+
+        [SerializeField] InputButton aimInput;
+
+        [SerializeField] float defaultZ;
+        [SerializeField] float zoomedZ;
+
+        [SerializeField] float speed = 9;
+
+        float actualZ;
+
+        public override void Execute()
+        {
+            if (!actualCameraTrans.value)
+                return;
+
+            float targetZ = defaultZ;
+
+            if (aimInput.isPressed)
+            {
+                targetZ = zoomedZ;
+            }
+
+            actualZ = Mathf.Lerp(actualZ, targetZ, speed * Time.deltaTime);
+
+            Vector3 targetPosition = Vector3.zero;
+            targetPosition.z = actualZ;
+            actualCameraTrans.value.localPosition = targetPosition;
+        }
+    }
+}
---
+++ Assets/Behaviour-Editor/Scripts/Behavior/Actions/InputButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace SA
+{
+    [CreateAssetMenu(menuName = "Inputs/Button")]
+    public class InputButton : Action
+    {
+        public string targetInput;
+        public bool isPressed;
+        public KeyState keyState;
+
+        public override void Execute()
+        {
+            switch (keyState)
+            {
+                case KeyState.onDown:
+                    isPressed = Input.GetButtonDown(targetInput);
+                    break;
+                case KeyState.onCurrent:
+                    isPressed = Input.GetButton(targetInput);
+                    break;
+                case KeyState.onUp:
+                    isPressed = Input.GetButtonUp(targetInput);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public enum KeyState
+        {
+            onDown, onCurrent, onUp
+        }
+    }
+}
