325e0703f211f8cf6f0b89a5b6a15ea319a10001
PsuperX <50513933+PsuperX@users.noreply.github.com>
2019-12-28 15:01:53 +0000

    Added Resources Manager
    Making my way into spawning weapons...

---
+++ Assets/Scripts/Manager/GameManagers.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace SA
+{
+    public static class GameManagers
+    {
+        static ResourcesManager resourcesManager;
+        public static ResourcesManager GetResourcesManager()
+        {
+            if (!resourcesManager)
+            {
+                resourcesManager = Resources.Load("Resources Manager") as ResourcesManager;
+                resourcesManager.Init();
+            }
+
+            return resourcesManager;
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/ResourcesManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SA
+{
+    [CreateAssetMenu(menuName = "Managers/Resources Manager")]
+    public class ResourcesManager : ScriptableObject
+    {
+        public List<Item> allItems = new List<Item>();
+        Dictionary<string, Item> itemDict = new Dictionary<string, Item>();
+
+        public void Init()
+        {
+            for (int i = 0; i < allItems.Count; i++)
+            {
+                if (!itemDict.ContainsKey(allItems[i].name))
+                {
+                    itemDict.Add(allItems[i].name, allItems[i]);
+                }
+                else
+                {
+                    Debug.Log("There's two items named: " + allItems[i].name + " ! That's ILLEGAL !");
+                }
+            }
+        }
+
+        public Item GetItemInstance(string targetID)
+        {
+            Item defaultItem = GetItem(targetID);
+            Item newItem = Instantiate(defaultItem);
+            newItem.name = defaultItem.name;
+
+            return newItem;
+        }
+
+        Item GetItem(string targetID)
+        {
+            itemDict.TryGetValue(targetID, out Item retVal);
+            return retVal;
+        }
+    }
+}
