0c00e230e62e376e07dcfbac6248e230c3fb46f3
PsuperX <50513933+PsuperX@users.noreply.github.com>
2019-12-29 15:52:25 +0000

    Weapons Sliders!
    - Added weapon sliders
    - Added glock
    - Fixed AK right hand IK Position

--- Assets/Scripts/Items/WeaponHook.cs
+++ Assets/Scripts/Items/WeaponHook.cs
@@ -12,6 +12,17 @@ namespace SA
         ParticleSystem[] particles;
         AudioSource audioSource;
 
+        [Header("Slider Settings")]
+        public Transform slider;
+        public AnimationCurve sliderCurve;
+        public float multiplier = 1;
+        public float sliderSpeed = 9;
+        Vector3 startPos;
+        float sliderT;
+
+        public bool isShooting;
+        bool initSliderLerp;
+
         public void Init()
         {
             GameObject go = new GameObject();
@@ -22,16 +33,46 @@ namespace SA
             audioSource.spatialBlend = 1;
 
             particles = GetComponentsInChildren<ParticleSystem>();
+
+            if(slider)
+            startPos = slider.localPosition;
         }
 
         public void Shoot()
         {
+            isShooting = true;
+
             if (particles != null)
             {
                 for (int i = 0; i < particles.Length; i++)
                 {
                     particles[i].Play();
                 }
+            }
+        }
+
+        private void Update()
+        {
+            if (isShooting)
+            {
+                if (!initSliderLerp)
+                {
+                    initSliderLerp = true;
+                    sliderT = 0;
+                }
+
+                sliderT += Time.deltaTime * sliderSpeed;
+                if(sliderT > 1)
+                {
+                    sliderT = 0;
+                    initSliderLerp = false;
+                    isShooting = false;
+                }
+
+                float targetZ = sliderCurve.Evaluate(sliderT) * multiplier;
+                Vector3 targetPos = startPos;
+                targetPos.z -= targetZ;
+                slider.transform.localPosition = targetPos;
             }
         }
     }
