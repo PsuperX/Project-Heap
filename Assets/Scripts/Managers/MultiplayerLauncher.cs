f1eb46782aabb84612a82a7c3bafa7d3d56c00f6
PsuperX <50513933+PsuperX@users.noreply.github.com>
2019-12-31 14:04:16 +0000

    Master Server Connection :D
    Finally we are connected to the master server :clap:

--- Assets/Scripts/Managers/MultiplayerLauncher.cs
+++ Assets/Scripts/Managers/MultiplayerLauncher.cs
@@ -1,22 +1,70 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace SA
 {
-    public class MultiplayerLauncher : MonoBehaviour
+    public class MultiplayerLauncher : MonoBehaviourPunCallbacks
     {
         delegate void OnSceneLoaded();
         bool isLoading;
 
         public static MultiplayerLauncher singleton;
 
+        public PunLogLevel logLevel;
+        public uint gameVersion = 1;
+
+        public SO.GameEvent onConnectingToMaster;
+        public SO.BoolVariable isConnected;
+
+
+        #region Init
         private void Awake()
         {
             if (!singleton)
                 singleton = this;
             else
                 Destroy(this.gameObject);
+        }
+
+        private void Start()
+        {
+            isConnected.value = false;
+            ConnectToServer();
+        }
+
+        void ConnectToServer()
+        {
+            PhotonNetwork.LogLevel = logLevel;
+            // PhotonNetwork.autoJoinLobby is gone.
+            PhotonNetwork.AutomaticallySyncScene = false;
+            PhotonNetwork.PhotonServerSettings.AppSettings.AppVersion = gameVersion.ToString();
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        #endregion
+
+        #region Photon Callbacks
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Connected Succesfully");
+            isConnected.value = true;
+            onConnectingToMaster.Raise();
+        }
+        
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            isConnected.value = false;
+            onConnectingToMaster.Raise();
+            Debug.Log("Disconnected with reason " + cause);
+        }
+        #endregion
+
+        #region Manager Methods
+        void LoadMainMenu()
+        {
+            StartCoroutine(LoadScene("MainMenu", OnMainMenu));
         }
 
         public void LoadCurrentRoom()
@@ -26,6 +74,7 @@ namespace SA
             if (!isLoading)
             {
                 isLoading = true;
+                Debug.Log("Loading scene: " + r.sceneName);
                 StartCoroutine(LoadScene(r.sceneName));
             }
         }
@@ -37,5 +86,17 @@ namespace SA
             if (callback != null)
                 callback.Invoke();
         }
+        #endregion
+
+        #region Setup Methods
+        void OnMainMenu()
+        {
+            isConnected.value = PhotonNetwork.IsConnected;
+            if (isConnected.value)
+                OnConnectedToMaster();
+            else
+                ConnectToServer();
+        }
+        #endregion
     }
 }
