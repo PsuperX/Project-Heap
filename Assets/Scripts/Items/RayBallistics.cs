207d3b4d88782f53a52ae266877eff7b69eabb94
PsuperX <50513933+PsuperX@users.noreply.github.com>
2020-03-20 14:02:36 +0000

    Ray Filtering
    Fix a bug where you could shoot yourself

--- Assets/Scripts/Items/RayBallistics.cs
+++ Assets/Scripts/Items/RayBallistics.cs
@@ -9,23 +9,30 @@ namespace SA
         {
             Vector3 origin = w.runtimeW.modelInstance.transform.position;
             Vector3 dir = states.movementValues.aimPosition;
-
             Ray ray = new Ray(origin, dir);
-            if (Physics.Raycast(ray, out RaycastHit hit, 100, states.ignoreLayers))
-            {
-                IHittable hittable = hit.transform.GetComponentInParent<IHittable>();
 
-                if (hittable == null)
-                {
-                    GameObject hitParticle = GameManagers.GetObjectPooler().RequestObject("bullet_hit");
-                    hitParticle.transform.position = hit.point;
-                    hitParticle.transform.rotation = Quaternion.LookRotation(hit.normal);
-                }
-                else
-                {
-                    hittable.OnHit(states, w, dir, hit.point, hit.normal);
-                }
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray, 100, states.ignoreLayers);
+            if (hits == null) return;
+            if (hits.Length == 0) return;
+
+            RaycastHit closestHit = GetClosestHit(origin, hits, states.photonID);
+            IHittable isHittable = closestHit.transform.GetComponentInParent<IHittable>();
+
+            if (isHittable == null)
+            {
+                // Do default hit behaviour
+                GameObject hitParticle = GameManagers.GetObjectPooler().RequestObject("bullet_hit");
+                Quaternion rot = Quaternion.LookRotation(closestHit.normal);
+                hitParticle.transform.position = closestHit.point;
+                hitParticle.transform.rotation = rot;
             }
+            else
+            {
+                // Execute IHittable custom behaviour
+                isHittable.OnHit(states, w, dir, origin, closestHit.normal);
+            }
+
 
             MultiplayerManager mm = MultiplayerManager.singleton;
             if (mm != null) // MultiplayerManager is null if is not on multiplayer
@@ -34,23 +41,59 @@ namespace SA
             }
         }
 
+        RaycastHit GetClosestHit(Vector3 origin, RaycastHit[] hits, int shooterID)
+        {
+            int closestIndex = 0;
+
+            float minDst = float.MaxValue;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                float tempDst = (hits[i].point - origin).sqrMagnitude;
+                if (tempDst < minDst)
+                {
+                    // Found a closer one
+                    StateManager states = hits[i].transform.GetComponentInParent<StateManager>();
+
+                    // Filter the shooter
+                    if (states != null)
+                    {
+                        if (states.photonID == shooterID)
+                            continue;
+                    }
+
+                    minDst = tempDst;
+                    closestIndex = i;
+                }
+            }
+
+            return hits[closestIndex];
+        }
+
         public void ClientShoot(StateManager states, Vector3 dir, Vector3 origin)
         {
+            Debug.Log("Client Shoot");
             Ray ray = new Ray(origin, dir);
-            if (Physics.Raycast(ray, out RaycastHit hit, 100))
-            {
-                IHittable hittable = hit.transform.GetComponentInParent<IHittable>();
 
-                if (hittable == null)
-                {
-                    GameObject hitParticle = GameManagers.GetObjectPooler().RequestObject("bullet_hit");
-                    hitParticle.transform.position = hit.point;
-                    hitParticle.transform.rotation = Quaternion.LookRotation(hit.normal);
-                }
-                else
-                {
-                    hittable.OnHit(states, states.inventory.curWeapon, dir, hit.point, hit.normal);
-                }
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(ray, 100, states.ignoreLayers);
+            if (hits == null) return;
+            if (hits.Length == 0) return;
+
+            RaycastHit closestHit = GetClosestHit(origin, hits, states.photonID);
+            IHittable isHittable = closestHit.transform.GetComponentInParent<IHittable>();
+
+            if (isHittable == null)
+            {
+                // Do default hit behaviour
+                GameObject hitParticle = GameManagers.GetObjectPooler().RequestObject("bullet_hit");
+                Quaternion rot = Quaternion.LookRotation(closestHit.normal);
+                hitParticle.transform.position = closestHit.point;
+                hitParticle.transform.rotation = rot;
+            }
+            else
+            {
+                // Execute IHittable custom behaviour
+                isHittable.OnHit(states, states.inventory.curWeapon, dir, origin, closestHit.normal);
             }
         }
     }
